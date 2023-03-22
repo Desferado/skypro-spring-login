5bdea5f0e84e766bd61eecbb7f78b128b4f022cb
Denis_vrn <denisfedorov50@gmail.com>
2023-03-22 23:10:03 +0300

    My homework 2.4

---
+++ src/main/java/pro/sky/skyproSpringLogin/SkyproSpringLoginApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.skyproSpringLogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproSpringLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproSpringLoginApplication.class, args);
+	}
+
+}
---
+++ test/java/pro/sky/skyproSpringLogin/SkyproSpringLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.skyproSpringLogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproSpringLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
